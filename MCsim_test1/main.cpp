eefc14ef691bdcc422620d96ba170a86a6b78d55
kazuki_kikuti <ery.eden@gmail.com>
2015-09-09 16:48:54 +0900

    test

---
+++ MCsim_test1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+
+
+using namespace Eigen;
+
+int main(){
+	MatrixXd m;
+	m = MatrixXd::Zero(2, 2);
+	std::cout << m << std::endl;
+
+	while (1);
+
+}
+
